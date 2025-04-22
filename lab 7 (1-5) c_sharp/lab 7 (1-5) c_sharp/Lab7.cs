b02c836131e296b9874746a5e577ccc4532e73a6
Matt_kmb_23 <154213225+Mattkmb@users.noreply.github.com>
2025-04-22 11:35:48 +0500

    Lab7.cs

--- lab 7 (1-5) c_sharp/lab 7 (1-5) c_sharp/Lab7.cs
+++ lab 7 (1-5) c_sharp/lab 7 (1-5) c_sharp/Lab7.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Xml.Serialization;
@@ -7,10 +7,9 @@ namespace Lab7Project
 {
     public class Lab7
     {
-        // ============================
-        // ЗАДАНИЕ 1. Текстовый файл
-        // (В файле найти сумму квадратов элементов)
-        // ============================
+        // ============================================================
+        // ЗАДАНИЕ 1. Текстовый файл – найти сумму квадратов элементов.
+        // ============================================================
         public static void FillTextFileTask1(string filename, int count)
         {
             Random rand = new Random();
@@ -36,32 +35,26 @@ namespace Lab7Project
                     sumOfSquares += (long)num * num;
                 }
             }
-            Console.WriteLine("Task1  : Сумма квадратов элементов = " + sumOfSquares);
+            Console.WriteLine("Task1: Сумма квадратов элементов = " + sumOfSquares);
         }
 
-        // ============================
-        // ЗАДАНИЕ 2. Текстовый файл
-        // (В файле вычислить произведение элементов)
-        // ============================
+        // ============================================================
+        // ЗАДАНИЕ 2. Текстовый файл – вычислить произведение элементов.
+        // ============================================================
         public static void FillTextFileTask2(string filename, int rows, int numbersPerRow)
         {
-            
             Random rand = new Random();
             using (StreamWriter sw = new StreamWriter(filename))
             {
                 for (int i = 0; i < rows; i++)
                 {
-                    string line = "";
+                    List<int> numbers = new List<int>();
                     for (int j = 0; j < numbersPerRow; j++)
                     {
-                        int num = rand.Next(1, 11);
-                        line += num.ToString();
-                        if (j < numbersPerRow - 1)
-                        {
-                            line += " ";
-                        }
+                        int num = rand.Next(1, 11); 
+                        numbers.Add(num);
                     }
-                    sw.WriteLine(line);
+                    sw.WriteLine(string.Join(" ", numbers));
                 }
             }
             Console.WriteLine($"Task2: Файл \"{filename}\" заполнен {rows} строками по {numbersPerRow} чисел.");
@@ -90,14 +83,12 @@ namespace Lab7Project
             if (!foundAny)
                 Console.WriteLine("Task2: В файле не найдено чисел.");
             else
-                Console.WriteLine("Task2  : Произведение элементов = " + product);
+                Console.WriteLine("Task2: Произведение элементов = " + product);
         }
 
-        // ============================
-        // ЗАДАНИЕ 3. Текстовый файл
-        // (Переписать в другой файл строки, имеющие заданную длину m)
-        // ============================
-        
+        // ============================================================
+        // ЗАДАНИЕ 3. Текстовый файл – переписать в другой файл строки, имеющие заданную длину m.
+        // ============================================================
         public static void FillTextFileTask3(string filename)
         {
             using (StreamWriter sw = new StreamWriter(filename))
@@ -122,13 +113,12 @@ namespace Lab7Project
                         sw.WriteLine(line);
                 }
             }
-            Console.WriteLine($"Task3  : Строки длиной {m} символов записаны в файл \"{outputFilename}\".");
+            Console.WriteLine($"Task3: Строки длиной {m} символов записаны в файл \"{outputFilename}\".");
         }
 
-        // ============================
-        // ЗАДАНИЕ 4. Бинарный файл
-        // (В бинарном файле вычислить произведение нечетных отрицательных чисел)
-        // ============================
+        // ============================================================
+        // ЗАДАНИЕ 4. Бинарный файл – вычислить произведение нечетных отрицательных чисел.
+        // ============================================================
         public static void FillBinaryFileTask4(string filename, int count)
         {
             Random rand = new Random();
@@ -140,7 +130,7 @@ namespace Lab7Project
                     bw.Write(num);
                 }
             }
-            Console.WriteLine($"Task4: Бинарный файл \"{filename}\" заполнен {count} случайными числами.");
+            Console.WriteLine($"Task4: Бинарный файл \"{filename}\" заполнен {count} числами.");
         }
 
         public static void Task4(string filename)
@@ -155,58 +145,75 @@ namespace Lab7Project
             bool found = false;
             foreach (int num in numbers)
             {
-                if (num < 0 && num % 2 != 0) 
+                if (num < 0 && num % 2 != 0)
                 {
                     product *= num;
                     found = true;
                 }
             }
             if (found)
-                Console.WriteLine("Task4  : Произведение нечетных отрицательных чисел = " + product);
+                Console.WriteLine("Task4: Произведение нечетных отрицательных чисел = " + product);
             else
-                Console.WriteLine("Task4  : Отрицательных нечетных чисел не найдено.");
+                Console.WriteLine("Task4: Отрицательных нечетных чисел не найдено.");
         }
 
-        // ============================
+        // ============================================================
         // ЗАДАНИЕ 5. Бинарные файлы и структуры (XML-сериализация)
-        // (Информация о багаже пассажира: найти число пассажиров с более чем 2 единицами багажа и число пассажиров, у которых количество единиц багажа выше среднего)
-        // ============================
+        // Информация о багаже пассажира.
+        // Найти:
+        // – число пассажиров с более чем 2 единицами багажа;
+        // – число пассажиров, у которых количество единиц багажа выше среднего.
+        // ============================================================
         [Serializable]
         public class BaggageItem
         {
-            public string ItemName { get; set; }
-            public double Mass { get; set; }
+            private string _itemName;
+            private double _mass;
+
+            public string ItemName
+            {
+                get { return _itemName; }
+                set { _itemName = value; }
+            }
+            public double Mass
+            {
+                get { return _mass; }
+                set { _mass = value; }
+            }
 
             
             public BaggageItem() { }
-
             public BaggageItem(string name, double mass)
             {
-                ItemName = name;
-                Mass = mass;
+                _itemName = name;
+                _mass = mass;
             }
         }
 
         [Serializable]
         public class PassengerBaggage
         {
-            public List<BaggageItem> Items { get; set; }
-            public PassengerBaggage()
+            private List<BaggageItem> _items;
+            public List<BaggageItem> Items
             {
-                Items = new List<BaggageItem>();
+                get { return _items; }
+                set { _items = value; }
             }
+            public PassengerBaggage() { _items = new List<BaggageItem>(); }
         }
 
         [Serializable]
         [XmlRoot("PassengersBaggage")]
         public class PassengersBaggageList
         {
+            private List<PassengerBaggage> _passengers;
             [XmlElement("Passenger")]
-            public List<PassengerBaggage> Passengers { get; set; }
-            public PassengersBaggageList()
+            public List<PassengerBaggage> Passengers
             {
-                Passengers = new List<PassengerBaggage>();
+                get { return _passengers; }
+                set { _passengers = value; }
             }
+            public PassengersBaggageList() { _passengers = new List<PassengerBaggage>(); }
         }
 
         public static void FillXMLFileTask5(string filename, int passengerCount, int minItems, int maxItems)
@@ -222,7 +229,7 @@ namespace Lab7Project
                 for (int j = 0; j < itemsCount; j++)
                 {
                     string name = possibleItems[rand.Next(possibleItems.Length)];
-                    double mass = Math.Round(rand.NextDouble() * 25 + 5, 1); // масса от 5 до 30 кг
+                    double mass = Math.Round(rand.NextDouble() * 25 + 5, 1);
                     passenger.Items.Add(new BaggageItem(name, mass));
                 }
                 pbList.Passengers.Add(passenger);
@@ -249,43 +256,40 @@ namespace Lab7Project
             int totalPassengers = pbList.Passengers.Count;
             foreach (PassengerBaggage passenger in pbList.Passengers)
             {
-                int items = passenger.Items.Count;
-                totalItems += items;
-                if (items > 2)
+                int itemsCount = passenger.Items.Count;
+                totalItems += itemsCount;
+                if (itemsCount > 2)
                     countMoreThanTwo++;
             }
-            double averageItems = totalPassengers > 0 ? (double)totalItems / totalPassengers : 0;
-
+            double avgItems = totalPassengers > 0 ? (double)totalItems / totalPassengers : 0;
             int countAboveAverage = 0;
             foreach (PassengerBaggage passenger in pbList.Passengers)
             {
-                if (passenger.Items.Count > averageItems)
+                if (passenger.Items.Count > avgItems)
                     countAboveAverage++;
             }
-            Console.WriteLine("Task5  :");
+            Console.WriteLine("Task5:");
             Console.WriteLine($"  Число пассажиров с более чем 2 единицами багажа = {countMoreThanTwo}");
-            Console.WriteLine($"  Число пассажиров, у которых количество багажа выше среднего ({averageItems:F2}) = {countAboveAverage}");
+            Console.WriteLine($"  Число пассажиров, у которых количество багажа выше среднего ({avgItems:F2}) = {countAboveAverage}");
         }
 
-        // ============================
-        // ЗАДАНИЕ 6. List
-        // (В списке L заменить первое вхождение списка L1 (если такое есть) на список L2)
-        // ============================
-        public static void Task6_List(List<int> L, List<int> L1, List<int> L2)
+        // ============================================================
+        // ЗАДАНИЕ 6. List – заменить первое вхождение подсписка L1 на список L2.
+        // ============================================================
+        public static void ReplaceFirstOccurrence<T>(List<T> L, List<T> L1, List<T> L2)
         {
             if (L1 == null || L1.Count == 0)
             {
                 Console.WriteLine("Task6: L1 пуст, нечего заменять.");
                 return;
             }
-
             int pos = -1;
             for (int i = 0; i <= L.Count - L1.Count; i++)
             {
                 bool match = true;
                 for (int j = 0; j < L1.Count; j++)
                 {
-                    if (L[i + j] != L1[j])
+                    if (!EqualityComparer<T>.Default.Equals(L[i + j], L1[j]))
                     {
                         match = false;
                         break;
@@ -304,87 +308,70 @@ namespace Lab7Project
             }
             L.RemoveRange(pos, L1.Count);
             L.InsertRange(pos, L2);
-            Console.Write("Task6  : Результирующий список: ");
+            Console.Write("Task6: Результирующий список: ");
             foreach (var item in L)
                 Console.Write(item + " ");
             Console.WriteLine();
         }
 
-        // ============================
-        // ЗАДАНИЕ 7. LinkedList
-        // (Сортировать элементы списка по возрастанию)
-        // ============================
-        public static void Task7_LinkedList(LinkedList<int> list)
+        // ============================================================
+        // ЗАДАНИЕ 7. LinkedList – сортировать элементы списка по возрастанию.
+        // ============================================================
+        public static void Task7_LinkedList<T>(LinkedList<T> list)
         {
             if (list == null || list.Count == 0)
             {
                 Console.WriteLine("Task7: LinkedList пуст.");
                 return;
             }
-            List<int> temp = new List<int>(list);
+            List<T> temp = new List<T>(list);
             temp.Sort();
-            Console.Write("Task7  : Отсортированный LinkedList: ");
+            Console.Write("Task7: Отсортированный LinkedList: ");
             foreach (var item in temp)
                 Console.Write(item + " ");
             Console.WriteLine();
         }
 
-        // ============================
-        // ЗАДАНИЕ 8. HashSet
-        // (Имеется перечень компьютерных игр. Для каждого студента задано, в какие игры он играет.
-        //  Определить:
-        //    – в какие игры из перечня играют все студенты,
-        //    – в какие игры играют некоторые студенты,
-        //    – в какие игры не играет ни один студент)
-        // ============================
+        // ============================================================
+        // ЗАДАНИЕ 8. HashSet – анализ перечня компьютерных игр.
+        // ============================================================
         public static void Task8_HashSet(List<string> masterGames, List<HashSet<string>> studentGames)
         {
-            
             HashSet<string> union = new HashSet<string>();
-        
-            HashSet<string> intersection = (studentGames.Count > 0) ? new HashSet<string>(studentGames[0]) : new HashSet<string>();
+            HashSet<string> intersection = studentGames.Count > 0 ? new HashSet<string>(studentGames[0]) : new HashSet<string>();
 
             foreach (var games in studentGames)
             {
                 union.UnionWith(games);
                 intersection.IntersectWith(games);
             }
-            // Вычисляем игры, в которые не играет ни один студент
             HashSet<string> notPlayed = new HashSet<string>(masterGames);
             notPlayed.ExceptWith(union);
 
-            // Сортировка результатов
             List<string> allStudents = new List<string>(intersection);
-            allStudents.Sort(StringComparer.CurrentCulture);
+            allStudents.Sort();
             List<string> someStudents = new List<string>(union);
-            someStudents.Sort(StringComparer.CurrentCulture);
+            someStudents.Sort();
             List<string> nonePlayed = new List<string>(notPlayed);
-            nonePlayed.Sort(StringComparer.CurrentCulture);
+            nonePlayed.Sort();
 
-            Console.WriteLine("Task8  :");
+            Console.WriteLine("Task8:");
             Console.WriteLine("  Игры, в которые играют все студенты: " + string.Join(", ", allStudents));
             Console.WriteLine("  Игры, в которые играют некоторые студенты: " + string.Join(", ", someStudents));
             Console.WriteLine("  Игры, в которые не играет ни один студент: " + string.Join(", ", nonePlayed));
         }
 
-        // ============================
-        // ЗАДАНИЕ 9. HashSet
-        // (Файл содержит текст на русском языке. Напечатать в алфавитном порядке все глухие согласные буквы,
-        // которые не входят хотя бы в одно слово)
-        // ============================
+        // ============================================================
+        // ЗАДАНИЕ 9. HashSet – Напечатать в алфавитном порядке все глухие согласные буквы,
+        // которые отсутствуют хотя бы в одном слове.
+        // ============================================================
         public static void Task9_HashSet(string filename)
         {
-            // Определим множество глухих согласных (в нижнем регистре).
-            // Используем следующие буквы: п, ф, к, т, с, ш, х, ц, ч.
             HashSet<char> voiceless = new HashSet<char> { 'п', 'ф', 'к', 'т', 'с', 'ш', 'х', 'ц', 'ч' };
-
             string text = File.ReadAllText(filename).ToLower();
-            // Разбиваем текст на слова (используем стандартные разделители)
             char[] delimiters = { ' ', '\n', '\r', ',', '.', ';', ':', '-', '!', '?', '(', ')', '"' };
             string[] words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-
             List<char> result = new List<char>();
-            // Для каждой глухой согласной проверяем: если хотя бы в одном слове её нет, то включаем в результат.
             foreach (char letter in voiceless)
             {
                 bool inEvery = true;
@@ -399,9 +386,8 @@ namespace Lab7Project
                 if (!inEvery)
                     result.Add(letter);
             }
-            result.Sort(); // Стандартная сортировка по коду символа (для русского это сработает, если используется Unicode).
-
-            Console.Write("Task9  : Глухие согласные, отсутствующие хотя бы в одном слове: ");
+            result.Sort();
+            Console.Write("Task9: Глухие согласные, отсутствующие хотя бы в одном слове: ");
             foreach (char letter in result)
                 Console.Write(letter + " ");
             Console.WriteLine();
